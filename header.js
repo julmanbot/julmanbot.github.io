4aeb7782fc69c7ba99e685177a80cea5c36d367f
MinecraftJulMan <juliano.m.mandrillon@gmail.com>
2021-01-02 14:22:31 +0100

    Ajout de la langue dans la description du compte

--- header.js
+++ header.js
@@ -11,10 +11,10 @@ $(window).on('load', function () {
             }
         }).then((res) => res.json())
             .then((json) => {
-                let { id, username, avatar, discriminator, premium_type } = json;
+                let { id, username, avatar, discriminator, premium_type, locale } = json;
                 if (id) {
                     $('ul.login-dropdown').children().first().html(`<img src="https://cdn.discordapp.com/avatars/${id}/${avatar}.png" width="24"></img>`);
-                    $('ul.login-dropdown').children().get(1).innerHTML = `${username}#${discriminator} - ${premium_type == 0 ? "Sans Discord Nitro 🛒" : premium_type == 2 ? "Avec Discord Nitro ✨" : "Avec Discord Nitro Classique 🎈"}`;
+                    $('ul.login-dropdown').children().get(1).innerHTML = `${username}#${discriminator} - ${premium_type == 0 ? "Sans Discord Nitro 🛒" : premium_type == 2 ? "Avec Discord Nitro ✨" : "Avec Discord Nitro Classique 🎈"} - Langue : ${locale}`;
                     $('ul.login-dropdown').children().get(1).classList.replace('css', 'no-css');
                     $('ul.login-dropdown').children().get(2).innerHTML = `<a href="https://julmanbot.github.io/?logout=true" style="color: red;">Se déconnecter</a>`;
                 }

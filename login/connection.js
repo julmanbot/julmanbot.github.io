4c7cf877ab333ff7fe6e1b490040aae719b8dc52
MinecraftJulMan <juliano.m.mandrillon@gmail.com>
2021-01-14 22:53:47 +0100

    update

--- login/connection.js
+++ login/connection.js
@@ -6,4 +6,4 @@
     Login from Discord for JulManBOT
 */
 
-!function(w){w.DiscordBot=class{constructor(client_id){this.client_id=client_id;}requestLogin(who,target){who.loginToBot(target,this);}};w.DiscordClient=class{constructor(){};loginToBot(target,bot){const a=document.body.appendChild(document.createElement('a'));a.href=`https://discord.com/api/oauth2/authorize?client_id=${bot.client_id}&redirect_uri=${target}&response_type=token&scope=identify&guilds`;adl("click",function(){},a);a.click();};};w.login=function(bot,who,target){bot.requestLogin(who,target);}}(window);
+!function(w){w.DiscordBot=class{constructor(client_id){this.client_id=client_id;}requestLogin(who,target){who.loginToBot(target,this);}};w.DiscordClient=class{constructor(){};loginToBot(target,bot){const a=document.body.appendChild(document.createElement('a'));a.href=`https://discord.com/api/oauth2/authorize?client_id=${bot.client_id}&redirect_uri=${target}&response_type=token&scope=identify%20guilds`;adl("click",function(){},a);a.click();};};w.login=function(bot,who,target){bot.requestLogin(who,target);}}(window);
